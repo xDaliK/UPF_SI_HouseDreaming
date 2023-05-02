567b43e7ebcc08c63781712778274f7419253431
Pedro Omedas <pedro.omedas@upf.edu>
2023-05-02 10:20:04 +0200

    Initial template project

---
+++ Assets/Scripts/tracking/PlayerMovement.cs
@@ -0,0 +1,25 @@
+using System.Collections;
+using System.Collections.Generic;
+using UnityEngine;
+
+public class PlayerMovement : MonoBehaviour
+{
+    [SerializeField] int playerIndex;
+    // Start is called before the first frame update
+    void Start()
+    {
+        
+    }
+
+    // Update is called once per frame
+    void Update()
+    {
+        
+    }
+
+    public void setPosition(Vector3 pos)
+    {
+        //swith playerIndex
+        transform.position = pos;
+    }
+}
