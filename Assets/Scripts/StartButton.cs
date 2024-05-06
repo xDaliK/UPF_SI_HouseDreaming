2f35d31a2fe3925c9ccc0988b1b1d71151db71c8
DaniLizarteCáceres <92033368+DanielLizarteCaceres@users.noreply.github.com>
2024-05-06 20:02:13 +0200

    Title Ver.1

---
+++ Assets/Scripts/StartButton.cs
@@ -0,0 +1,39 @@
+using System.Collections;
+using System.Collections.Generic;
+using UnityEngine;
+using UnityEngine.EventSystems;
+using UnityEngine.SceneManagement;
+
+public class StartButton : MonoBehaviour
+{
+
+    private AudioSource audioSource;
+    private Renderer renderer;
+    private Color originalColor;
+
+    void Awake()
+    {
+        audioSource = GetComponent<AudioSource>();
+        renderer = GetComponent<Renderer>();
+        originalColor = renderer.material.color;
+    }
+
+    void OnTriggerEnter(Collider other)
+    {
+        if (other.gameObject.name.StartsWith("Player"))
+        {
+
+            renderer.material.color = originalColor * 0.5f;
+
+            audioSource.Play();
+
+
+            Invoke("LoadScene", 2f);
+        }
+    }
+
+    void LoadScene()
+    {
+        SceneManager.LoadScene("SceneXavi");
+    }
+}
