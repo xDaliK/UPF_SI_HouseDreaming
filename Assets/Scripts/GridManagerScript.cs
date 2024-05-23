5b877e0d164ba8d9509403389cfedfb35fae12a1
DaniLizarteCáceres <92033368+DanielLizarteCaceres@users.noreply.github.com>
2024-05-23 20:29:47 +0200

    Movement+Grid(init)

---
+++ Assets/Scripts/GridManagerScript.cs
@@ -0,0 +1,103 @@
+using System.Collections;
+using System.Collections.Generic;
+using UnityEngine;
+namespace InteractiveSystemsTemplate
+
+{
+
+
+
+    public class GridManagerScript : MonoBehaviour
+    {
+        public int gridWidth = 10;
+        public int gridHeight = 10;
+        public float gridSize = 10;
+        public Vector3[,] grid;
+
+        // public GameObject gridCellPrefab; // El prefab de la celda de la cuadrícula
+
+
+
+
+        void Awake()
+        {
+            // Inicializa la cuadrícula
+            grid = new Vector3[gridWidth, gridHeight];
+            for (int x = 0; x < gridWidth; x++)
+            {
+                for (int y = 0; y < gridHeight; y++)
+                {
+                    Vector3 cellPosition = new Vector3(x * gridSize, 0, y * gridSize);
+                    grid[x, y] = cellPosition;
+
+                    // No necesitas instanciar un prefab para cada celda de la cuadrícula
+                    // Instantiate(gridCellPrefab, cellPosition, Quaternion.identity);
+                }
+            }
+        }
+
+
+
+
+
+        public void DropObject(GameObject obj, Vector3 position)
+        {
+            // Encuentra el cuadrado más cercano en la cuadrícula
+            Vector3 closestGridPosition = FindClosestGridPosition(position);
+
+            // Mueve el objeto a la posición del cuadrado más cercano
+            obj.transform.position = closestGridPosition;
+        }
+
+
+        public Vector3 FindClosestGridPosition(Vector3 position)
+        {
+            if (grid == null)
+            {
+                Debug.LogError("La cuadrícula no está inicializada");
+                Awake();
+            }
+
+
+            Debug.Log("holafind 0");
+            Vector3 closestGridPosition = grid[0, 0];
+            Debug.Log("holafind 1");
+            float closestDistance = Vector3.Distance(position, closestGridPosition);
+            Debug.Log("find 1");
+
+            for (int x = 0; x < gridWidth; x++)
+            {
+                for (int y = 0; y < gridHeight; y++)
+                {
+                    Debug.Log("find 2");
+                    float distance = Vector3.Distance(position, grid[x, y]);
+                    if (distance < closestDistance)
+                    {
+                        Debug.Log("find 3");
+                        closestGridPosition = grid[x, y];
+                        closestDistance = distance;
+                    }
+                }
+            }
+
+            return closestGridPosition;
+        }
+
+
+        void OnDrawGizmos()
+        {
+            Gizmos.color = Color.white;
+            for (int x = 0; x <= gridWidth; x++)
+            {
+                for (int y = 0; y <= gridHeight; y++)
+                {
+                    Gizmos.DrawLine(new Vector3(x * gridSize, 0, 0), new Vector3(x * gridSize, 0, gridHeight * gridSize));
+                    Gizmos.DrawLine(new Vector3(0, 0, y * gridSize), new Vector3(gridWidth * gridSize, 0, y * gridSize));
+                }
+            }
+        }
+
+
+
+    }
+}
--- Assets/Scripts/ObjectGrabbed.cs
+++ Assets/Scripts/ObjectGrabbed.cs
@@ -1,71 +1,90 @@
 using System.Collections;
 using System.Collections.Generic;
 using UnityEngine;
-
-public class ObjectGrabbed : MonoBehaviour
+namespace InteractiveSystemsTemplate
 {
-    private AudioSource audioSource;
-    private Renderer renderer;
-    private Color originalColor;
-    private Transform playerTransform;
-    private bool isHeld = false;
-    private Vector3 offset;
-    private float timeOverObject = 0f; // Tiempo que el jugador ha estado sobre el objeto
-    private float releaseHeight = 1.8f; // Altura a la que se suelta el objeto
-
-    void Awake()
+    public class ObjectGrabbed : MonoBehaviour
     {
-        audioSource = GetComponent<AudioSource>();
-        renderer = GetComponent<Renderer>();
-        originalColor = renderer.material.color;
-    }
+        private AudioSource audioSource;
+        private Renderer renderer;
+        private Color originalColor;
+        private Transform playerTransform;
+        private bool isHeld = false;
+        private Vector3 offset;
+        private float timeOverObject = 0f; // Tiempo que el jugador ha estado sobre el objeto
+        private float releaseHeight = 0.5f; // Altura a la que se suelta el objeto
 
-    void OnTriggerEnter(Collider other)
-    {
-        if (other.gameObject.name.StartsWith("Player"))
+        public GridManagerScript gridManager;
+
+
+        void Awake()
         {
-            renderer.material.color = originalColor * 0.75f;
-            playerTransform = other.transform;
+            audioSource = GetComponent<AudioSource>();
+            renderer = GetComponent<Renderer>();
+            originalColor = renderer.material.color;
         }
-    }
 
-    void OnTriggerExit(Collider other)
-    {
-        if (other.gameObject.name.StartsWith("Player"))
+        void OnTriggerEnter(Collider other)
         {
-            renderer.material.color = originalColor;
-            playerTransform = null;
-            timeOverObject = 0f; // Resetea el contador cuando el jugador sale del objeto
-        }
-    }
-
-    void Update()
-    {
-        if (playerTransform != null && !isHeld) // Solo cuenta el tiempo si el objeto no está siendo agarrado
-        {
-            timeOverObject += Time.deltaTime; // Incrementa el contador de tiempo
-            if (timeOverObject >= 2f) // Si el jugador ha estado sobre el objeto durante 2 segundos
+            if (other.gameObject.name.StartsWith("Player"))
             {
-                isHeld = true; // Agarra el objeto
-                // Calcula el desplazamiento entre el jugador y el objeto
-                offset = transform.position - playerTransform.position;
-                timeOverObject = 0f; // Resetea el contador cuando el objeto es agarrado
+                Debug.Log(renderer.material.color);
+                renderer.material.color = originalColor * 0.5f; //el material cambia de color en el inspector pero no en la renderización TODO
+                Debug.Log(renderer.material.color);
+
+                playerTransform = other.transform;
             }
         }
 
-        if (isHeld)
+        void OnTriggerExit(Collider other)
         {
-            // Si el jugador está a una altura de 1.8 metros, suelta el objeto
-            if (playerTransform.position.y >= releaseHeight)
+            if (other.gameObject.name.StartsWith("Player"))
             {
-                isHeld = false;
+                renderer.material.color = originalColor;
+                playerTransform = null;
+                timeOverObject = 0f; // Resetea el contador cuando el jugador sale del objeto
             }
-            else
+        }
+
+        void Update()
+        {
+            if (playerTransform != null && !isHeld) // Solo cuenta el tiempo si el objeto no está siendo agarrado
             {
-                // Mueve el objeto a la posición del jugador más el desplazamiento
-                Vector3 newPosition = playerTransform.position + offset;
-                newPosition.y = transform.position.y; // Mantén la altura constante
-                transform.position = newPosition;
+                timeOverObject += Time.deltaTime; // Incrementa el contador de tiempo
+                if (timeOverObject >= 2f) // Si el jugador ha estado sobre el objeto durante 2 segundos
+                {
+                    isHeld = true; // Agarra el objeto
+                    // Calcula el desplazamiento entre el jugador y el objeto
+                    offset = transform.position - playerTransform.position;
+                    timeOverObject = 0f; // Resetea el contador cuando el objeto es agarrado
+                }
+            }
+
+            if (isHeld)
+            {
+                // Si el jugador está a una altura de 1.8 metros, suelta el objeto
+                Debug.Log(transform.position);
+                Vector3 closestGridPosition1 = gridManager.FindClosestGridPosition(transform.position);
+                Debug.Log(closestGridPosition1);
+                if (playerTransform.position.y >= releaseHeight)
+                {
+                    isHeld = false;
+
+                    // Encuentra la celda de la cuadrícula más cercana
+                    Debug.Log("find -1");
+                    Vector3 closestGridPosition2 = gridManager.FindClosestGridPosition(transform.position);
+                    Debug.Log(closestGridPosition2);
+
+                    // Mueve el mueble a la posición de la celda más cercana
+                    transform.position = transform.position; //deberia ser hacia closestGridPostion2 TODO (closestGridPosition2 siempre es 0,0,0).
+                }
+                else
+                {
+                    // Mueve el objeto a la posición del jugador más el desplazamiento
+                    Vector3 newPosition = playerTransform.position + offset;
+                    newPosition.y = transform.position.y; // Mantén la altura constante
+                    transform.position = newPosition;
+                }
             }
         }
     }
