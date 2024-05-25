2af3bba577d6cac0db7216786d4cc77e64eba8d8
DaniLizarteCáceres <92033368+DanielLizarteCaceres@users.noreply.github.com>
2024-05-25 22:51:22 +0200

    Update Movement + Verification

--- Assets/Scripts/ObjectGrabbed.cs
+++ Assets/Scripts/ObjectGrabbed.cs
@@ -1,111 +1,144 @@
-using System.Collections;
+ï»¿using System.Collections;
 using System.Collections.Generic;
 using UnityEngine;
-namespace InteractiveSystemsTemplate
+
+public class ObjectGrabbed : MonoBehaviour
 {
-    public class ObjectGrabbed : MonoBehaviour
+    private AudioSource audioSource;
+    private Renderer renderer;
+    private Color originalColor;
+    private Transform playerTransform;
+    private bool isHeld = false;
+    private Vector3 offset;
+    private float timeOverObject = 0f; // Tiempo que el jugador ha estado sobre el objeto
+    private float releaseHeight = 5f; // Altura a la que se suelta el objeto
+    private string objectHeld;
+    private Vector3 lastKnownPlayerPosition;
+
+    void Awake()
     {
+        audioSource = GetComponent<AudioSource>();
+        renderer = GetComponent<Renderer>();
+        originalColor = renderer.material.color;
+    }
 
-        private AudioSource audioSource;
-        private Renderer renderer;
-        private Color originalColor;
-        private Transform playerTransform;
-        private bool isHeld = false;
-        private Vector3 offset;
-        private float timeOverObject = 0f; // Tiempo que el jugador ha estado sobre el objeto
-        private float releaseHeight = 0.1f; // Altura a la que se suelta el objeto
-
-        public GridManagerScript gridManager;
-
-        private float positionplayerx = -9999.9f;
-        private float positionplayery = -9999.9f;
-        private float positionplayerz = -9999.9f;
-        private string objectHeld;
-
-        void Awake()
+    void OnTriggerEnter(Collider other)
+    {
+        if (other.gameObject.name.StartsWith("Player"))
         {
-            audioSource = GetComponent<AudioSource>();
-            renderer = GetComponent<Renderer>();
-            originalColor = renderer.material.color;
-        }
-
-        void OnTriggerEnter(Collider other)
-        {
-            if (other.gameObject.name.StartsWith("Player"))
-            {
-                //Debug.Log(renderer.material.color);
-                renderer.material.color = originalColor * 0.5f; //el material cambia de color en el inspector pero no en la renderización TODO
-                //Debug.Log(renderer.material.color);
-
-                playerTransform = other.transform;
-            }
-        }
-
-        void OnTriggerExit(Collider other)
-        {
-            if (other.gameObject.name.StartsWith("Player"))
-            {
-                renderer.material.color = originalColor;
-                playerTransform = null;
-                timeOverObject = 0f; // Resetea el contador cuando el jugador sale del objeto
-            }
-        }
-
-        void Update()
-        {
-
-            positionplayerx = playerTransform.position.x;
-            positionplayery = playerTransform.position.y;
-            positionplayerz = playerTransform.position.z;
-
-
-            if (playerTransform != null && !isHeld) // Solo cuenta el tiempo si el objeto no está siendo agarrado
-            {
-                timeOverObject += Time.deltaTime; // Incrementa el contador de tiempo
-                if (timeOverObject >= 2f) // Si el jugador ha estado sobre el objeto durante 2 segundos
-                {
-                    isHeld = true; // Agarra el objeto
-                    // Calcula el desplazamiento entre el jugador y el objeto
-                    objectHeld = gameObject.tag;
-                    offset = transform.position - playerTransform.position;
-                    timeOverObject = 0f; // Resetea el contador cuando el objeto es agarrado
-                }
-            }
-
-            if (isHeld)
-            {
-                // Si el jugador está a una altura de 1.8 metros, suelta el objeto
-                if (positionplayery >= releaseHeight)
-                {
-                    isHeld = false;
-                    Debug.Log("Soltado1");
-
-
-                    // Mueve el mueble a la posición de la celda más cercana
-                    transform.position = transform.position;
-                    Debug.Log("Soltado2");
-
-                    Debug.Log(gameObject.tag);
-                    // Comprueba si el objeto es un sofá y está en la posición correcta
-                    if (objectHeld == "sofa" && Mathf.Abs(transform.position.x - 48.7f) <= 5 && Mathf.Abs(transform.position.z - 21.8f) <= 5)
-                    {
-                        Debug.Log("Sofá correctamente colocado");
-                    }
-                    else if (LayerMask.LayerToName(gameObject.layer) == "sofa")
-                    {
-                        Debug.Log("Sofá mal colocado");
-                    }
-                }
-                else
-                {
-                    // Mueve el objeto a la posición del jugador más el desplazamiento
-                    Vector3 newPosition = playerTransform.position + offset;
-                    newPosition.y = transform.position.y; // Mantén la altura constante
-                    transform.position = newPosition;
-                }
-            }
-
+            renderer.material.color = originalColor * 0.75f;
+            playerTransform = other.transform;
+            Debug.Log("Player ha entrado en el trigger.");
+            lastKnownPlayerPosition = playerTransform.position;
         }
     }
 
+    void OnTriggerExit(Collider other)
+    {
+        if (other.gameObject.name.StartsWith("Player"))
+        {
+            renderer.material.color = originalColor;
+            timeOverObject = 0f; // Resetea el contador cuando el jugador sale del objeto
+            Debug.Log("Player ha salido del trigger.");
+            lastKnownPlayerPosition = playerTransform.position;
+            playerTransform = null;
+        }
+    }
+
+    void Update()
+    {
+        Debug.Log("Cogido: " + isHeld);
+
+        if (playerTransform != null && !isHeld) // Solo cuenta el tiempo si el objeto no estï¿½ siendo agarrado
+        {
+            timeOverObject += Time.deltaTime; // Incrementa el contador de tiempo
+            Debug.Log("Tiempo sobre objeto: " + timeOverObject);
+            if (timeOverObject >= 2f) // Si el jugador ha estado sobre el objeto durante 2 segundos
+            {
+                isHeld = true; // Agarra el objeto
+                // Calcula el desplazamiento entre el jugador y el objeto
+                objectHeld = gameObject.tag;
+                offset = transform.position - playerTransform.position;
+                timeOverObject = 0f; // Resetea el contador cuando el objeto es agarrado
+                Debug.Log("Objeto agarrado: " + objectHeld);
+            }
+        }
+
+        if (isHeld)
+        {
+
+            // Si el jugador estï¿½ a una altura de 1.8 metros, suelta el objeto
+            Debug.Log("Altura: " + lastKnownPlayerPosition.y);
+            if (lastKnownPlayerPosition.y >= releaseHeight)
+            {
+                isHeld = false;
+
+                Debug.Log("Soltado Objeto");
+
+                // Realiza la comprobaciÃ³n de posiciÃ³n solo cuando el objeto se suelta
+                if (objectHeld == "sofa" && Mathf.Abs(transform.position.x - 48.7f) <= 5 && Mathf.Abs(transform.position.z - 21.8f) <= 5)
+                {
+                    Debug.Log("SofÃ¡ correctamente colocado");
+                }
+                else if (objectHeld == "sofa")
+                {
+                    Debug.Log("SofÃ¡ mal colocado");
+                }
+
+                if (objectHeld == "bed" && Mathf.Abs(transform.position.x - 84.17352f) <= 5 && Mathf.Abs(transform.position.z - 54.87921f) <= 5)
+                {
+                    Debug.Log("Cama correctamente colocada");
+                }
+                else if (objectHeld == "bed")
+                {
+                    Debug.Log("Cama mal colocada");
+                }
+
+                if (objectHeld == "plant" && Mathf.Abs(transform.position.x - (35.7f)) <= 10 && Mathf.Abs(transform.position.z - 83.16f) <= 10)
+                {
+                    Debug.Log("Planta correctamente colocada");
+                }
+                else if (objectHeld == "plant")
+                {
+                    Debug.Log("Planta mal colocada");
+                }
+
+                if (objectHeld == "cabinet" && Mathf.Abs(transform.position.x - (51.3f)) <= 5 && Mathf.Abs(transform.position.z - 52.72921f) <= 5)
+                {
+                    Debug.Log("Cabinet correctamente colocado");
+                }
+                else if (objectHeld == "cabinet")
+                {
+                    Debug.Log("Cabinet mal colocado");
+                }
+
+                if (objectHeld == "bookcase" && Mathf.Abs(transform.position.x - (19.77352f)) <= 5 && Mathf.Abs(transform.position.z - 47.37921f) <= 5)
+                {
+                    Debug.Log("Bookcase correctamente colocado");
+                }
+                else if (objectHeld == "bookcase")
+                {
+                    Debug.Log("Bookcase mal colocado");
+                }
+
+                if (objectHeld == "television" && Mathf.Abs(transform.position.x - (32.5f)) <= 5 && Mathf.Abs(transform.position.z - 23.4f) <= 5)
+                {
+                    Debug.Log("Television correctamente colocado");
+                }
+                else if (objectHeld == "television")
+                {
+                    Debug.Log("Television mal colocado");
+                }
+            }
+
+            else
+            {
+                // Mueve el objeto a la posiciï¿½n del jugador mï¿½s el desplazamiento
+                Vector3 newPosition = playerTransform.position + offset;
+                newPosition.y = transform.position.y; // Mantï¿½n la altura constante
+                transform.position = newPosition;
+                Debug.Log("PosiciÃ³n del objeto actualizada a: " + newPosition);
+            }
+        }
+    }
 }
